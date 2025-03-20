3c0783dbe4bc0cb053af93e553a539f4e16c9d4b
yansoe353 <yannaingsoe.itech@gmail.com>
2025-03-20 10:57:05 +0700

    Update useVideoEditor.ts

--- src/hooks/useVideoEditor.ts
+++ src/hooks/useVideoEditor.ts
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg/dist/ffmpeg.min.js';
+import { createFFmpeg, fetchFile } from 'https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.7/dist/umd/ffmpeg.min.js';
+
 
 
 const ffmpeg = createFFmpeg({ log: true });
